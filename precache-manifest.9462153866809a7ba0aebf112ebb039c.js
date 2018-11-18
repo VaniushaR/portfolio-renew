8c20db4bfe5e2c548103736531b607a5cf225165
VaniushaR <vaniusha.ra@gmail.com>
2018-11-17 19:41:55 -0600

    Updates

---
+++ precache-manifest.9462153866809a7ba0aebf112ebb039c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-renew/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed251ec59e7a6b52610f",
+    "url": "/portfolio-renew/static/js/runtime~main.ed251ec5.js"
+  },
+  {
+    "revision": "3b44de6d2433bf10cffb",
+    "url": "/portfolio-renew/static/js/main.3b44de6d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/portfolio-renew/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "3b44de6d2433bf10cffb",
+    "url": "/portfolio-renew/static/css/main.a78862fd.chunk.css"
+  },
+  {
+    "revision": "98a4d8f19857a8990b61098de39581a2",
+    "url": "/portfolio-renew/index.html"
+  }
+];
